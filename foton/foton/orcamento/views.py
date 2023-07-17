40a950e89b33a038a8e20a4be3599b6e7d8f071f
LAMP-LUCAS <lamp.lucas@live.com>
2023-07-16 23:06:03 -0300

    deletando antiga estrutura

--- foton/foton/orcamento/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from .models import Orcamento
-
-def orcamento_list(request):
-    orcamento = Orcamento.objects.all()
-    return render(request, 'orcamento/orcamento_list.html', {'orcamento': orcamento})
