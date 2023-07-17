d509943a102ff480151b74d88ec8f43db556e1fe
LAMP-LUCAS <lamp.lucas@live.com>
2023-07-16 23:04:24 -0300

    agora irei tentar modificar a estrutura do projeto

--- foton/foton/aplicacoes.bat
+++ foton/foton/aplicacoes.bat
@@ -1,3 +1,3 @@
-python manage.py startapp orcamento
-python manage.py startapp contratos
-python manage.py startapp clientes
+python manage.py startapp gestao_contrato
+python manage.py startapp gestao_cliente
+python manage.py startapp gestao_orcamento
