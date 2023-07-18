fa8a1e6e7c4669566411392ebdbbe4d5addb4ef7
LAMP-LUCAS <lamp.lucas@live.com>
2023-07-18 19:30:38 -0300

    confirmando modificações

--- foton_system/foton/gestao_contrato/urls.py
+++ foton_system/foton/gestao_contrato/urls.py
@@ -7,12 +7,7 @@ urlpatterns = [
     path('', views.index, name='home_contrato'),
     path('detalhes/<int:contrato_id>/', views.detalhes, name='detalhes'),
     path('excluir/<int:contrato_id>/', views.excluir, name='excluir'),
-<<<<<<< HEAD
-    path('incluir/<int:contrato_id>/', views.incluir, name='incluir'),
-    # outras rotas do microsserviço de contrato...
-=======
     path('editar/<int:contrato_id>/', views.editar, name='editar'),
     path('incluir/', views.incluir, name='incluir'),
     # outras URLs do microsserviço de contrato...
->>>>>>> master
 ]
