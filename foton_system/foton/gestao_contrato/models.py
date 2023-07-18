18e9898313fce3a09562671f766b3afcf28e8f40
LAMP-LUCAS <lamp.lucas@live.com>
2023-07-18 20:50:48 -0300

    correção de migrações com o banco de dados

--- foton_system/foton/gestao_contrato/models.py
+++ foton_system/foton/gestao_contrato/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Contrato(models.Model):
     titulo = models.CharField(max_length=100)
     data = models.DateField()
-    numero = models.CharField(max_length=100)
+    numero = models.CharField(
+        max_length=100,
+        default=0,
+        )
 
     def get_cliente(self):
         from gestao_cliente.models import Cliente
@@ -13,8 +16,19 @@ class Contrato(models.Model):
         from gestao_orcamento.models import Orcamento
         return Orcamento.objects.get(id=self.orcamento_id)
 
-    cliente = models.ForeignKey('gestao_cliente.Cliente', on_delete=models.CASCADE, related_name='contratos')
-    orcamento = models.OneToOneField('gestao_orcamento.Orcamento', on_delete=models.CASCADE, related_name='contrato')
+    cliente = models.ForeignKey(
+        'gestao_cliente.Cliente',
+        on_delete=models.CASCADE,
+        related_name='n_contrato',
+        default=1
+        )
+    
+    orcamento = models.OneToOneField(
+        'gestao_orcamento.Orcamento',
+        on_delete=models.CASCADE,
+        related_name='id_contrato',
+        default=1,
+        )
 
     def __str__(self):
         return self.titulo
