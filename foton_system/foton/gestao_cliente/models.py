b241c6c101007053b2b20ad578ea8903010dd69f
LAMP-LUCAS <lamp.lucas@live.com>
2023-07-18 19:37:44 -0300

    correção 02

--- foton_system/foton/gestao_cliente/models.py
+++ foton_system/foton/gestao_cliente/models.py
@@ -3,8 +3,7 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
-    contrato = models.ForeignKey('gestao_contrato.Contrato', on_delete=models.SET_NULL, null=True, blank=True)
-
+    contrato = models.ForeignKey('gestao_contrato.Contrato', on_delete=models.SET_NULL, null=True, blank=True, related_name='clientes')
     # outros campos do cliente
 
     def __str__(self):
